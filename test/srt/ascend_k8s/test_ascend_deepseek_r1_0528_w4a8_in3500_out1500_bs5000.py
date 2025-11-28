59ffa97120916cf7d3e5793052c39bcca9fde158
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-11-28 17:36:30 +0800

    Update test_ascend_deepseek_r1_0528_w4a8_in3500_out1500_bs5000.py

--- test/srt/ascend_k8s/test_ascend_deepseek_r1_0528_w4a8_in3500_out1500_bs5000.py
+++ test/srt/ascend_k8s/test_ascend_deepseek_r1_0528_w4a8_in3500_out1500_bs5000.py
@@ -2,14 +2,14 @@ import unittest
 
 from test_ascend_single_mix_utils import (
     TestSingleMixUtils,
-    QWEN3_235B_MODEL_PATH,
-    QWEN3_235B_OTHER_ARGS,
-    QWEN3_235B_ENVS,
+    DEEPSEEK_R1_0528_W4A8_MODEL_PATH,
+    DEEPSEEK_R1_0528_W4A8_OTHER_ARGS,
+    DEEPSEEK_R1_0528_W4A8_ENVS,
 )
 
 
-class TestQwen3_3235B(TestSingleMixUtils):
-    model = QWEN3_235B_MODEL_PATH
+class TestDEEPSEEK_R1_0528_W4A8(TestSingleMixUtils):
+    model = DEEPSEEK_R1_0528_W4A8_MODEL_PATH
     dataset = (
         "/data/ascend-ci-share-pkking-sglang/modelscope/hub/datasets/Qwen3-235B-A22B-W8A8/GSM8K-in2048-bs5000/test.jsonl")
     other_args = QWEN3_235B_OTHER_ARGS
@@ -20,7 +20,7 @@ class TestQwen3_3235B(TestSingleMixUtils):
     tpot = 100
     output_token_throughput = 300
 
-    def test_qwen3_235b(self):
+    def test_deepseek_r1_0528_w4a8(self):
         self.run_ais_bench()
 
 
