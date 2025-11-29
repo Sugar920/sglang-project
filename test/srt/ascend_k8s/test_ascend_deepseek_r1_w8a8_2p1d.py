5539f9c73ca763ceec1d411b9901b0978ec30d9f
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-11-29 13:01:57 +0800

    Create test_ascend_deepseek_r1_w8a8_2p1d.py

---
+++ test/srt/ascend_k8s/test_ascend_deepseek_r1_w8a8_2p1d.py
@@ -0,0 +1,167 @@
+import unittest
+
+from test_ascend_disaggregation_utils import (
+    TestAscendDisaggregationUtils,
+)
+
+MODEL_PATH = "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/DeepSeek-R1-0528-w4a8"
+MODEL_PATH = "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/Howeee/DeepSeek-R1-0528-w8a8"
+
+DEEPSEEK_R1_CONFIG = {
+    "model_path": MODEL_PATH,
+    "prefill_envs": {
+        "SGLANG_USE_MLAPO": "1",
+        "SGLANG_USE_FIA_NZ": "1",
+        # "ENABLE_MOE_NZ": "1",
+        "SGLANG_USE_AG_AFTER_QLORA": "1",
+        "HCCL_BUFFSIZE": "1536",
+        "TASK_QUEUE_ENABLE": "2",
+        "SGLANG_SET_CPU_AFFINITY": "1",
+        "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+        "STREAMS_PER_DEVICE": "32",
+        "HCCL_SOCKET_IFNAME": "lo",
+        "GLOO_SOCKET_IFNAME": "lo",
+    },
+    "decode_envs": {
+        "SGLANG_USE_MLAPO": "1",
+        "SGLANG_USE_FIA_NZ": "1",
+        # "ENABLE_MOE_NZ": "1",
+        # "NO_DP_ALL_GATHER": "1",
+        # "ENABLE_FUSED_MOE": "1",
+        "DP_ROUND_ROBIN": "1",
+        "HCCL_BUFFSIZE": "1200",
+        "SGLANG_SET_CPU_AFFINITY": "1",
+        "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+        "STREAMS_PER_DEVICE": "32",
+        # "HCCL_SOCKET_IFNAME": "data0.3001",
+        # "GLOO_SOCKET_IFNAME": "data0.3001",
+    },
+    "prefill_args": [
+        "--quantization",
+        "w8a8_int8",
+        "--disaggregation-mode",
+        "prefill",
+        "--nnodes",
+        1,
+        "--node-rank",
+        0,
+        "--tp-size",
+        16,
+        "--dp-size",
+        2,
+        "--mem-fraction-static",
+        0.81,
+        "--disable-radix-cache",
+        "--chunked-prefill-size",
+        32768,
+        "--max-prefill-tokens",
+        28680,
+        "--max-running-requests",
+        8,
+        "--context-length",
+        8192,
+        "--disable-overlap-schedule",
+        "--moe-a2a-backend",
+        "deepep",
+        "--deepep-mode",
+        "normal",
+        # "--ep-dispatch-algorithm",
+        # "static",
+        # "--init-expert-location",
+        # "/home/fuyong/codes/sglang/sgl_ascend/hot_map/aisbench_hot_map.pt",
+        "--enable-dp-attention",
+        # "--tokenizer-worker-num",
+        # 4,
+        "--speculative-algorithm",
+        "NEXTN",
+        "--speculative-num-steps",
+        1,
+        "--speculative-eagle-topk",
+        1,
+        "--speculative-num-draft-tokens",
+        2,
+        # "--enable-expert-distribution-metrics",
+        "--disable-shared-experts-fusion",
+        "--disable-cuda-graph",
+    ],
+    "decode_args": [
+        "--quantization",
+        "w8a8_int8",
+        "--disaggregation-mode",
+        "decode",
+        "--tp-size",
+        32,
+        "--dp-size",
+        32,
+        "--mem-fraction-static",
+        0.9,
+        "--moe-a2a-backend",
+        "deepep",
+        "--enable-dp-attention",
+        "--deepep-mode",
+        "low_latency",
+        "--enable-dp-lm-head",
+        "--moe-dense-tp-size",
+        1,
+        "--disable-cuda-graph",
+        "--watchdog-timeout",
+        9000,
+        "--context-length",
+        8192,
+        "--max-running-requests",
+        768,
+        "--prefill-round-robin-balance",
+        "--cuda-graph-bs",
+        1,
+        2,
+        4,
+        6,
+        8,
+        10,
+        12,
+        14,
+        16,
+        18,
+        20,
+        22,
+        24,
+        # "--ep-dispatch-algorithm",
+        # "static",
+        # "--init-expert-location",
+        # "/data/.cache/hot_map/aisbench_hot_map_decode.pt",
+        # "--ep-num-redundant-experts",
+        # 64,
+        "--tokenizer-worker-num",
+        4,
+        "--speculative-algorithm",
+        "NEXTN",
+        "--speculative-num-steps",
+        1,
+        "--speculative-eagle-topk",
+        1,
+        "--speculative-num-draft-tokens",
+        2,
+        # "--enable-expert-distribution-metrics",
+        "--disable-shared-experts-fusion",
+    ],
+}
+
+
+class TestQwen3_30B_A3B(TestSingleMixUtils):
+    model = QWEN3_30B_A3B_MODEL_PATH
+    dataset = (
+        "/data/ascend-ci-share-pkking-sglang/modelscope/hub/datasets/Qwen3-32B-w8a8-MindIE/GSM8K-in3500-bs5000/test.jsonl")
+    other_args = QWEN3_30B_A3B_OTHER_ARGS
+    envs = QWEN3_30B_A3B_ENVS
+    max_out_len = 300
+    batch_size = 16
+    num_prompts = int(batch_size) * 4
+    tpot = 100
+    output_token_throughput = 300
+
+    def test_qwen3_30b_a3b(self):
+        self.run_ais_bench()
+
+
+if __name__ == "__main__":
+    unittest.main()
