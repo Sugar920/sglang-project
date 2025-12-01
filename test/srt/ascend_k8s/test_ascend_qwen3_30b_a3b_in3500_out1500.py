d6e0f350766a3941c9216040ddcdb18f493ff603
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-01 15:59:33 +0800

    Update test_ascend_qwen3_30b_a3b_in3500_out1500.py

--- test/srt/ascend_k8s/test_ascend_qwen3_30b_a3b_in3500_out1500.py
+++ test/srt/ascend_k8s/test_ascend_qwen3_30b_a3b_in3500_out1500.py
@@ -10,8 +10,6 @@ from test_ascend_single_mix_utils import (
 
 class TestQwen3_30B_A3B(TestSingleMixUtils):
     model = QWEN3_30B_A3B_MODEL_PATH
-    dataset = (
-        "/data/ascend-ci-share-pkking-sglang/modelscope/hub/datasets/Qwen3-32B-w8a8-MindIE/GSM8K-in3500-bs5000/test.jsonl")
     other_args = QWEN3_30B_A3B_OTHER_ARGS
     envs = QWEN3_30B_A3B_ENVS
     max_out_len = 1500
@@ -21,7 +19,7 @@ class TestQwen3_30B_A3B(TestSingleMixUtils):
     output_token_throughput = 300
 
     def test_qwen3_30b_a3b(self):
-        self.run_ais_bench()
+        self.run_throughput()
 
 
 if __name__ == "__main__":
