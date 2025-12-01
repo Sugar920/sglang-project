58d3024abf8f17f673a5d2421d718802ad4bc85d
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-01 15:58:25 +0800

    Update test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quarot_in16k_out10k.py

--- test/srt/ascend_k8s/test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quarot_in16k_out10k.py
+++ test/srt/ascend_k8s/test_ascend_qwen3_coder_480b_a35b_instruct_w8a8_quarot_in16k_out10k.py
@@ -10,18 +10,20 @@ from test_ascend_single_mix_utils import (
 
 class TestQwen3_Coder_480B_A35b_Instruct_W8a8_Quarot(TestSingleMixUtils):
     model = QWEN3_CODER_480B_A35B_INSTRUCT_W8A8_QUAROT_MODEL_PATH
-    dataset = (
-        "/data/ascend-ci-share-pkking-sglang/modelscope/hub/datasets/Qwen3-32B-w8a8-MindIE/GSM8K-in3500-bs5000/test.jsonl")
     other_args = QWEN3_CODER_480B_A35B_INSTRUCT_W8A8_QUAROT_OTHER_ARGS
     envs = QWEN3_CODER_480B_A35B_INSTRUCT_W8A8_QUAROT_ENVS
-    max_out_len = 1500
-    batch_size = 48
-    num_prompts = int(batch_size) * 4
-    tpot = 100
-    output_token_throughput = 300
+    dataset_name = "random"
+    request_rate = 5.5
+    max_concurrency = 16
+    input_len = 16000
+    output_len = 10000
+    random_range_ratio = 0.5
+    ttft = 1206.81
+    tpot = 36.45
+    output_token_throughput = 252
 
     def test_qwen3_coder_480b_a35b_instruct_w8a8_quarot(self):
-        self.run_ais_bench()
+        self.run_throughput()
 
 
 if __name__ == "__main__":
