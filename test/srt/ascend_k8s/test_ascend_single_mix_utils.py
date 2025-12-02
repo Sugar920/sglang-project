85633eda45c00aeaee84c54929ae3a0edce7dbaf
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-02 21:42:14 +0800

    Update test_ascend_single_mix_utils.py

--- test/srt/ascend_k8s/test_ascend_single_mix_utils.py
+++ test/srt/ascend_k8s/test_ascend_single_mix_utils.py
@@ -272,7 +272,7 @@ def run_command(cmd, shell=True):
         return None
 
 def run_bench_serving(host, port, dataset_name="random", request_rate=8, max_concurrency=8, input_len=1024, output_len=1024,
-                      random_range_ratio=0.5):
+                      random_range_ratio=1):
     num_prompts = max_concurrency * 4
     command = (f"python3 -m sglang.bench_serving --backend sglang --host {host} --port {port} --dataset-name {dataset_name} --request-rate {request_rate} "
                f"--max-concurrency {max_concurrency} --num-prompts {num_prompts} --random-input-len {input_len} "
