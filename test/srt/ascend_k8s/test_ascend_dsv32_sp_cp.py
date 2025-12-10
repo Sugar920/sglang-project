e54416da57913a21e658d4e56d9680b8495205a5
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-10 18:02:12 +0800

    Update test_ascend_dsv32_sp_cp.py

--- test/srt/ascend_k8s/test_ascend_dsv32_sp_cp.py
+++ test/srt/ascend_k8s/test_ascend_dsv32_sp_cp.py
@@ -1,10 +1,21 @@
+import os
 import unittest
+from types import SimpleNamespace
+from urllib.parse import urlparse
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
+from sglang.test.test_utils import (
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
 
 from test_ascend_disaggregation_utils import (
     TestAscendDisaggregationUtils,
 )
 
-MODEL_PATH = "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/Howeee/DeepSeek-R1-0528-w8a8"
+MODEL_PATH = "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/DeepSeek-V3.2-Exp-W8A8"
 
 MODEL_CONFIG = {
     "model_path": MODEL_PATH,
@@ -132,21 +143,44 @@ MODEL_CONFIG = {
 }
 
 
-class Test_DeepSeek_R1_W8A8_2P1D_In3500_Out1500(TestAscendDisaggregationUtils):
-    model_config = MODEL_CONFIG
-    dataset_name = "random"
-    request_rate = 16
-    max_concurrency = 8
-    num_prompts = int(max_concurrency) * 4
-    input_len = 3500
-    output_len = 1500
-    random_range_ratio = 1
-    ttft = 3167.46
-    tpot = 46.18
-    output_token_throughput = 12663.9
 
-    def test_throughput(self):
-        self.run_throughput()
+
+TEST_MODEL_MATRIX = {
+    "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/DeepSeek-V3.2-Exp-W8A8": {
+        "accuracy": 0.95,
+        "latency": 1000,
+    },
+}
+
+
+class TestAscendEnableMixedChunk(TestAscendDisaggregationUtils):
+
+    @classmethod
+    def setUpClass(cls):
+
+    def test_a_gsm8k(self):
+        with self.subTest(model=MODEL_PATH):
+            print(f"##=== Testing accuracy: {model} ===##")
+
+
+            try:
+                args = SimpleNamespace(
+                    num_shots=20,
+                    data_path=None,
+                    num_questions=200,
+                    max_new_tokens=512,
+                    parallel=64,
+                    host=f"http://192.168.0.60",
+                    port=6688,
+                )
+
+                metrics = run_eval_few_shot_gsm8k(args)
+#                self.assertGreaterEqual(
+ #                   metrics["accuracy"],
+  #                  TEST_MODEL_MATRIX[model]["accuracy"],
+   #             )
+            finally:
+                pass
 
 
 if __name__ == "__main__":
