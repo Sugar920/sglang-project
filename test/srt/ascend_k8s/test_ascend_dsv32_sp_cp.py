722d46bf83716b0f8f6e7ee360efd291a2dfea74
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-11 10:59:00 +0800

    Update test_ascend_dsv32_sp_cp.py

--- test/srt/ascend_k8s/test_ascend_dsv32_sp_cp.py
+++ test/srt/ascend_k8s/test_ascend_dsv32_sp_cp.py
@@ -17,7 +17,7 @@ from test_ascend_disaggregation_utils import (
 
 MODEL_PATH = "/data/ascend-ci-share-pkking-sglang/modelscope/hub/models/DeepSeek-V3.2-Exp-W8A8"
 
-MODEL_CONFIG = {
+MODEL_CONFIG_OPEN = {
     "model_path": MODEL_PATH,
     "prefill_envs": {
         "SGLANG_SET_CPU_AFFIMITY": "1",
@@ -136,7 +136,120 @@ MODEL_CONFIG = {
     ],
 }
 
-
+MODEL_CONFIG_CLOSE = {
+    "model_path": MODEL_PATH,
+    "prefill_envs": {
+        "SGLANG_SET_CPU_AFFIMITY": "1",
+        "LD_LIBRARY_PATH": "/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}",
+        "ASCEND_HOME_PATH": "/usr/local/Ascend/ascend-toolkit/latest",
+        "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+        "STREAMS_PER_DEVICE": "32",
+        "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "16",
+        "HCCL_BUFFSIZE": "2800",
+        "HAS_INDEX_K": "1",
+        "SGLANG_DEEPEP_BF16_DISPATCH": "0",
+        "SGLANG_NPU_USE_MLAPO": "0",
+        "SGLANG_USE_AG_AFTER_QLORA": "0",
+        "USE_MULTI_STREAM": "1",
+        "ENABLE_MOE_NZ": "1",
+        "PROFILING_MODE": "dynamic",
+        "HCCL_OP_EXPANSION_MODE": "AIV",
+        "HCCL_SOCKET_IFNAME": "enp23s0f3",
+        "GLOO_SOCKET_IFNAME": "enp23s0f3",    
+    },
+    "decode_envs": {
+        "SGLANG_SET_CPU_AFFIMITY": "1",
+        "LD_LIBRARY_PATH": "/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}",
+        "ASCEND_HOME_PATH": "/usr/local/Ascend/ascend-toolkit/latest",
+        "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+        "STREAMS_PER_DEVICE": "32",
+        "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "16",
+        "HCCL_BUFFSIZE": "1024",
+        "HAS_INDEX_K": "1",
+        "SGLANG_DEEPEP_BF16_DISPATCH": "0",
+        "SGLANG_NPU_USE_MLAPO": "0",
+        "SGLANG_USE_AG_AFTER_QLORA": "0",
+        "USE_MULTI_STREAM": "1",
+        "ENABLE_FUSED_MOE": "1",
+        "HCCL_OP_EXPANSION_MODE": "AIV",
+        "TASK_QUEUE_ENABLE": "0",
+        "DEEP_NORMAL_MODE_USE_INT8_QUANT": "1",
+        "HCCL_SOCKET_IFNAME": "enp23s0f3",
+        "GLOO_SOCKET_IFNAME": "enp23s0f3",     
+    },
+    "prefill_args": [
+        "--nnodes",
+        2,
+        "--disaggregation-mode",
+        "prefill",
+        "--tp",
+        32,
+        "--watchdog-timeout",
+        9000,
+        "--mem-fraction-static",
+        0.85,
+        "--max-total-tokens",
+        68000,
+        "--context-length",
+        68000,
+        "--disable-radix-cache",
+        "--chunked-prefill-size",
+        327680,
+        "--max-prefill-tokens",
+        68000,
+        "--max-running-requests",
+        "16",
+        "--moe-a2a-backend",
+        "deepep",
+        "--deepep-mode",
+        "auto",
+        "--quantization",
+        "w8a8_int8",
+        "--disable-cuda-graph",      
+    ],
+    "decode_args": [
+        "--nnodes",
+        "2",
+        "--disaggregation-mode",
+        "decode",
+        "--tp",
+        32,
+        "--dp",
+        4,
+        "--ep",
+        32,
+        "--moe-dense-tp-size",
+        1,
+        "--enable-dp-attention",
+        "--enable-dp-lm-head",
+        "--watchdog-timeout",
+        9000,
+        "--mem-fraction-static",
+        0.85,
+        "--max-total-tokens",
+        68000,
+        "--context-length",
+        68000,
+        "--disable-radix-cache",
+        "--chunked-prefill-size",
+        327680,
+        "--max-prefill-tokens",
+        68000,
+        "--max-running-requests",
+        "128",
+        "--cuda-graph-max-bs",
+        "32",
+        "--moe-a2a-backend",
+        "deepep",
+        "--deepep-mode",
+        "low_latency",
+        "--quantization",
+        "w8a8_int8",
+        "--prefill-round-robin-balance",  
+        "--load-balance-method",
+        "round_robin",
+    ],
+}
 
 
 TEST_MODEL_MATRIX = {
@@ -147,28 +260,30 @@ TEST_MODEL_MATRIX = {
 }
 
 
-class TestAscendEnableMixedChunk(TestAscendDisaggregationUtils):
-    def test_a_gsm8k(self):
-        with self.subTest(model=MODEL_PATH):
+class TestAscendEnableSpAndCp(TestAscendDisaggregationUtils):
+    def run_gsm8k(self, model_config):
+        with self.subTest(model=model_config.get("model_path")):
             print(f"##=== Testing accuracy: {model} ===##")
-            try:
-                args = SimpleNamespace(
-                    num_shots=20,
-                    data_path=None,
-                    num_questions=200,
-                    max_new_tokens=512,
-                    parallel=64,
-                    host=f"http://192.168.0.60",
-                    port=6688,
-                )
+            args = SimpleNamespace(
+                num_shots=20,
+                data_path=None,
+                num_questions=200,
+                max_new_tokens=512,
+                parallel=64,
+                host=f"http://127.0.0.1",
+                port=6688,
+            )
+            metrics = run_eval_few_shot_gsm8k(args)
+            return metrics["accuracy"]
+    
+    def test_close(self):
+        accuracy_close = self.run_gsm8k(MODEL_CONFIG_CLOSE)
 
-                metrics = run_eval_few_shot_gsm8k(args)
-#                self.assertGreaterEqual(
- #                   metrics["accuracy"],
-  #                  TEST_MODEL_MATRIX[model]["accuracy"],
-   #             )
-            finally:
-                pass
+    def test_open(self):
+        accuracy_open = self.run_gsm8k(MODEL_CONFIG_OPEN)
+        if accuracy_open < accuracy_close:
+            accuracy_diff = (accuracy_close - accuracy_open) / accuracy_close
+        self.assertLessEqual(accuracy_diff, 0.01)
 
 
 if __name__ == "__main__":
