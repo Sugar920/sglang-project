7e38bc82b11af67a4a9d8681b7e6dfe181119a96
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-11 17:56:25 +0800

    Update test_ascend_enable_mixed_chunk2.py

--- test/srt/ascend/test_ascend_enable_mixed_chunk2.py
+++ test/srt/ascend/test_ascend_enable_mixed_chunk2.py
@@ -97,7 +97,7 @@ class TestAscendEnableMixedChunk(CustomTestCase):
                         port=int(self.url.port),
                     )
 
-                    metrics_with_mixed_chunk = run_eval_few_shot_gsm8k(args)
+                    self.metrics_with_mixed_chunk = run_eval_few_shot_gsm8k(args)
                 finally:
                     kill_process_tree(process.pid)
 
@@ -121,16 +121,19 @@ class TestAscendEnableMixedChunk(CustomTestCase):
                         port=int(self.url.port),
                     )
 
-                    metrics_without_mixed_chunk = run_eval_few_shot_gsm8k(args)
+                    self.metrics_without_mixed_chunk = run_eval_few_shot_gsm8k(args)
+                
+
+                self.accuracy_with_mixed_chunk = metrics_with_mixed_chunk["accuracy"]
+                self.accuracy_without_mixed_chunk = metrics_without_mixed_chunk["accuracy"]
+                if self.accuracy_with_mixed_chunk < self.accuracy_without_mixed_chunk"
+                    accuracy_diff = (self.accuracy_without_mixed_chunk - self.accuracy_with_mixed_chunk) / self.accuracy_without_mixed_chunk
+                    self.assertLessEqual(
+                       accuracy_diff,
+                       0.01,
+                   )
                 finally:
                     kill_process_tree(process.pid)
-
-                # accuracy_with_mixed_chunk = metrics_with_mixed_chunk["accuracy"]
-                # accuracy_without_mixed_chunk = metrics_without_mixed_chunk["accuracy"]
-                self.assertGreaterEqual(
-                       metrics_with_mixed_chunk["accuracy"],
-                       metrics_without_mixed_chunk["accuracy"],
-                   )
 
 
 if __name__ == "__main__":
