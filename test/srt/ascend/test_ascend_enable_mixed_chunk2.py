b366cb873e5d17fc243f599a6c33ace300a7068f
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-12 09:31:37 +0800

    Update test_ascend_enable_mixed_chunk2.py

--- test/srt/ascend/test_ascend_enable_mixed_chunk2.py
+++ test/srt/ascend/test_ascend_enable_mixed_chunk2.py
@@ -20,36 +20,14 @@ TEST_MODEL_MATRIX = {
 
 
 class TestAscendEnableMixedChunk(CustomTestCase):
-    metrics_with_mixed_chunk = None
     metrics_without_mixed_chunk = None
+    metrics_with_mixed_chunk = None
 
     @classmethod
     def setUpClass(cls):
         cls.models = TEST_MODEL_MATRIX.keys()
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.url = urlparse(DEFAULT_URL_FOR_TEST)
-
-        cls.common_args_with_mixed_chunk = [
-            "--trust-remote-code",
-            "--attention-backend",
-            "ascend",
-            "--mem-fraction-static",
-            "0.8",
-            "--disable-radix-cache",
-            "--chunked-prefill-size",
-            "-1",
-            "--tp-size",
-            "4",
-            "--enable-mixed-chunk",
-            "--max-running-requests",
-            4,
-            "--cuda-graph-bs",
-            1,
-            2,
-            3,
-            4,
-            "--disable-radix-cache",
-        ]
 
         cls.common_args_without_mixed_chunk = [
             "--trust-remote-code",
@@ -63,7 +41,29 @@ class TestAscendEnableMixedChunk(CustomTestCase):
             "--tp-size",
             "4",
             "--max-running-requests",
+            16,
+            "--cuda-graph-bs",
+            1,
+            2,
+            3,
             4,
+            "--disable-radix-cache",
+        ]
+        
+        cls.common_args_with_mixed_chunk = [
+            "--trust-remote-code",
+            "--attention-backend",
+            "ascend",
+            "--mem-fraction-static",
+            "0.8",
+            "--disable-radix-cache",
+            "--chunked-prefill-size",
+            "-1",
+            "--tp-size",
+            "4",
+            "--enable-mixed-chunk",
+            "--max-running-requests",
+            16,
             "--cuda-graph-bs",
             1,
             2,
@@ -84,30 +84,6 @@ class TestAscendEnableMixedChunk(CustomTestCase):
         for model in self.models:
             with self.subTest(model=model):
                 print(f"##=== Testing accuracy: {model} ===##")
-
-                process_with_mixed_chunk = popen_launch_server(
-                    model,
-                    self.base_url,
-                    timeout=1500,
-                    other_args=[
-                        *self.common_args_with_mixed_chunk,
-                    ],
-                )
-
-                try:
-                    args = SimpleNamespace(
-                        num_shots=5,
-                        data_path=None,
-                        num_questions=1319,
-                        max_new_tokens=512,
-                        parallel=256,
-                        host=f"http://{self.url.hostname}",
-                        port=int(self.url.port),
-                    )
-
-                    self.metrics_with_mixed_chunk = run_eval_few_shot_gsm8k(args)
-                finally:
-                    kill_process_tree(process_with_mixed_chunk.pid)
 
                 process_without_mixed_chunk = popen_launch_server(
                     model,
@@ -130,10 +106,34 @@ class TestAscendEnableMixedChunk(CustomTestCase):
                     )
 
                     self.metrics_without_mixed_chunk = run_eval_few_shot_gsm8k(args)
-                    print("metrics_with_mixed_chunk: ", self.metrics_with_mixed_chunk)
+                finally:
+                    kill_process_tree(process_without_mixed_chunk.pid)
+
+                process_with_mixed_chunk = popen_launch_server(
+                    model,
+                    self.base_url,
+                    timeout=1500,
+                    other_args=[
+                        *self.common_args_with_mixed_chunk,
+                    ],
+                )
+
+                try:
+                    args = SimpleNamespace(
+                        num_shots=5,
+                        data_path=None,
+                        num_questions=1319,
+                        max_new_tokens=512,
+                        parallel=256,
+                        host=f"http://{self.url.hostname}",
+                        port=int(self.url.port),
+                    )
+
+                    self.metrics_with_mixed_chunk = run_eval_few_shot_gsm8k(args)
                     print("metrics_without_mixed_chunk: ", self.metrics_without_mixed_chunk)
-                    accuracy_with_mixed_chunk = self.metrics_with_mixed_chunk["accuracy"]
+                    print("metrics_with_mixed_chunk: ", self.metrics_with_mixed_chunk)
                     accuracy_without_mixed_chunk = self.metrics_without_mixed_chunk["accuracy"]
+                    accuracy_with_mixed_chunk = self.metrics_with_mixed_chunk["accuracy"]
                     if accuracy_with_mixed_chunk < accuracy_without_mixed_chunk:
                         accuracy_diff = (accuracy_without_mixed_chunk - accuracy_with_mixed_chunk) / accuracy_without_mixed_chunk
                         self.assertLessEqual(
@@ -141,7 +141,7 @@ class TestAscendEnableMixedChunk(CustomTestCase):
                            0.01,
                        )
                 finally:
-                    kill_process_tree(process_without_mixed_chunk.pid)
+                    kill_process_tree(process_with_mixed_chunk.pid)
 
 
 if __name__ == "__main__":
